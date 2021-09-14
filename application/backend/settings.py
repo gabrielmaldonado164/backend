288bc4f093a1f4981ee9ec1dc1748536c4d75ea1
Lucas Rios Ventosa <riosventosalucas@gmail.com>
2021-09-14 17:09:35 -0300

    added installed

--- application/backend/settings.py
+++ application/backend/settings.py
@@ -75,9 +75,16 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'backend',
+        'USER': 'root',
+        'PASSWORD': 'satancojealpapa',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
