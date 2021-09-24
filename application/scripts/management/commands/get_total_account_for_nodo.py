e4ed0535ead01cc661b4b360bd5382a7210dbdaa
Lucas Rios Ventosa <lucasr@towebs.com>
2021-09-24 18:02:35 -0300

    fix total account and script checker

--- application/scripts/management/commands/get_total_account_for_nodo.py
+++ application/scripts/management/commands/get_total_account_for_nodo.py
@@ -12,24 +12,24 @@ from   schemas.models.nodo         import Nodo
 class Script(object):
     
     def do(self):
-    	nodos = Nodo.objects.all()
+        nodos = Nodo.objects.all()
 
-    	for nodo in nodos:
+        for nodo in nodos:
 
-    		print("[ INFO ] Obteniendo datos de {nodo}".format(nodo=nodo.name))
+            print("[ INFO ] Obteniendo datos de {nodo}".format(nodo=nodo.name))
 
-    		try:
-    			response = requests.get('http://{nodo}:8000/api/v1/get_total_accounts/'.format(nodo=nodo.name))
-				response = response.json()
+            try:
+                response = requests.get('http://{nodo}:8000/api/v1/get_total_accounts/'.format(nodo=nodo.name))
+                response = response.json()
 
-				total    = response['response']
+                total    = response['response']
 
-				nodo.total_accounts = total
-				nodo.save()
+                nodo.total_accounts = total
+                nodo.save()
 
-				print("\t[ OK ] Total de sitios en {nodo}: {total}".format(nodo=nodo, total=total))
-			except Exception as e:
-				print("\t[ ERROR ] {error}".format(e))
+                print("\t[ OK ] Total de sitios en {nodo}: {total}".format(nodo=nodo, total=total))
+            except Exception as e:
+                print("\t[ ERROR ] {error}".format(e))
 
 
 
