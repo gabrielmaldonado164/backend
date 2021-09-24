cb84e4ebbc90edd7f57e44278d2dc7901b20fc7e
Lucas Rios Ventosa <lucasr@towebs.com>
2021-09-24 18:00:04 -0300

    added total account and script checker

--- application/backend/settings.py
+++ application/backend/settings.py
@@ -7,7 +7,7 @@ database = Database()
 BASE_DIR   = Path(__file__).resolve().parent.parent
 print(socket.gethostname())
 HOSTNAME   = socket.gethostname()
-DEV_HOST   = ['lucas-linux', 'POLN02147', 'lucas-linux3','twcentos']
+DEV_HOST   = ['lucas-linux', 'POLN02147', 'lucas-linux3','twcentos', 'lucas-notebook']
 SECRET_KEY = 'django-insecure-ugd817-)yo#e&h*3###(=)n#=998q$$j5u7$bj6ogh)9(4v1$-'
 
 if HOSTNAME in DEV_HOST:
---
+++ application/scripts/management/commands/get_total_account_for_nodo.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Python
+import requests
+
+# Django
+from   django.core.management.base import BaseCommand
+
+# App
+from   schemas.models.nodo         import Nodo
+
+class Script(object):
+    
+    def do(self):
+    	nodos = Nodo.objects.all()
+
+    	for nodo in nodos:
+
+    		print("[ INFO ] Obteniendo datos de {nodo}".format(nodo=nodo.name))
+
+    		try:
+    			response = requests.get('http://{nodo}:8000/api/v1/get_total_accounts/'.format(nodo=nodo.name))
+				response = response.json()
+
+				total    = response['response']
+
+				nodo.total_accounts = total
+				nodo.save()
+
+				print("\t[ OK ] Total de sitios en {nodo}: {total}".format(nodo=nodo, total=total))
+			except Exception as e:
+				print("\t[ ERROR ] {error}".format(e))
+
+
+
+class Command(BaseCommand):
+    def __init__(self):
+        super(Command, self).__init__()
+
+    def handle(self, *args, **options):
+        worker = Script()
+        worker.do()    
+
