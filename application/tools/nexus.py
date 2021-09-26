d7325db36a16dedd8b486b2fec8d805fc363fe56
Lucas Rios Ventosa <riosventosalucas@gmail.com>
2021-09-26 01:29:49 -0300

    fix get hostname

--- application/tools/nexus.py
+++ application/tools/nexus.py
@@ -16,7 +16,7 @@ class Nexus(object):
 		hostname = socket.gethostname()
 
 		aux = ''
-		for i in range(len(hostname.split('.')) +1):
+		for i in range(1, len(hostname.split('.'))):
 			aux += hostname.split('.')[i] + '.'
 		return "nodo1.{}".format(aux[:-1])
 		
