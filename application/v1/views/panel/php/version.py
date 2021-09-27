888b4e97927bd7ebb176c506698ae7a317af71cc
Lucas Rios Ventosa <riosventosalucas@gmail.com>
2021-09-26 21:51:48 -0300

    added change php version

--- application/v1/views/panel/php/version.py
+++ application/v1/views/panel/php/version.py
@@ -77,5 +77,65 @@ class PhpVersionApiView(APIView):
         
         return Response(response)
 
+    def post(self,request, format=None):
+        req = request.POST
+
+        domain   = req.get('domain')
+        username = req.get('username')
+        version  = req.get('version')
+        nexus    = Nexus()
+        
+        if not domain:
+            response = {
+                'status'  : False,
+                'message' : 'No domain found.',
+                'response': None,
+                'error'   : '[ ERROR ] No domain provided'
+            }
+
+            return Response(response)
+
+        if not username:
+            response = {
+                'status'  : False,
+                'message' : 'No username found.',
+                'response': None,
+                'error'   : '[ ERROR ] No username provided'
+            }
+
+            return Response(response)
+
+        if not version:
+            response = {
+                'status'  : False,
+                'message' : 'No version found.',
+                'response': None,
+                'error'   : '[ ERROR ] No version provided'
+            }
+
+            return Response(response)
+
+        server = Nodo.objects.get(name=nexus.get_account_server(domain=domain))
+        
+        if server:
+            params = {
+                'username' : username,
+                'version'  : version
+            }
+
+            response = requests.get('http://{nodo}:8000/api/v1/panel/php/change_php_version/'.format(nodo=server.name), params=params)
+            
+        else:
+            response = {
+                'status'  : False,
+                'message' : 'No server found.',
+                'response': None,
+                'error'   : '[ ERROR ] No server provided'
+            }
+
+        response = response.json()
+        
+        return Response(response)
+
 
 
