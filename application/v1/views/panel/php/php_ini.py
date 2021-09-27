4791e09bc05eb4fbaf1fda097a93157a5daffff8
Lucas Rios Ventosa <riosventosalucas@gmail.com>
2021-09-26 21:46:40 -0300

    added put

--- application/v1/views/panel/php/php_ini.py
+++ application/v1/views/panel/php/php_ini.py
@@ -159,5 +159,63 @@ class PhpIniApiView(APIView):
         
         return Response(response)
 
+    def put(self, request, format=None):
+        req = request.POST
 
+        domain          = req.get('domain')
+        username        = req.get('username')
+        version         = req.get('version')
+        nexus           = Nexus()
+        
+        if not domain:
+            response = {
+                'status'  : False,
+                'message' : 'No domain found.',
+                'response': None,
+                'error'   : '[ ERROR ] No domain provided'
+            }
 
+            return Response(response)
+
+        if not version:
+            response = {
+                'status'  : False,
+                'message' : 'No version found.',
+                'response': None,
+                'error'   : '[ ERROR ] No version provided'
+            }
+
+            return Response(response)
+
+        if not username:
+            response = {
+                'status'  : False,
+                'message' : 'No username found.',
+                'response': None,
+                'error'   : '[ ERROR ] No username provided'
+            }
+
+            return Response(response)
+
+        server = Nodo.objects.get(name=nexus.get_account_server(domain=domain))
+        
+        if server:
+            params = {
+                'username' : username,
+                'version'  : version,
+                'php_ini'  : php_ini
+            }
+
+            response = requests.put('http://{nodo}:8000/api/v1/panel/php/php_ini/'.format(nodo=server.name), data=params)
+            
+        else:
+            response = {
+                'status'  : False,
+                'message' : 'No username found.',
+                'response': None,
+                'error'   : '[ ERROR ] No username provided'
+            }
+
+        response = response.json()
+        
+        return Response(response)
